38ceb27111614df7bbc9216e97b526aaae88c74e
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-08 18:18:52 +0000

    add prepend method to linkedlist class and verify it works

--- linkedList/linkedlist.js
+++ linkedList/linkedlist.js
@@ -9,4 +9,15 @@ class Linkedlist {
   constructor() {
     this.head = null;
   }
+
+  //insert node at the start of list
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
 }
+
+const linkedlist = new Linkedlist();
+linkedlist.prepend('test1');
+console.log(linkedlist);
