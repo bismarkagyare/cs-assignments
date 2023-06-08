ca7087eeec9e927e7fbfedbe7bb91fce8c232d66
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-08 21:10:36 +0000

    add append method

--- linkedList/linkedlist.js
+++ linkedList/linkedlist.js
@@ -16,8 +16,27 @@ class Linkedlist {
     newNode.next = this.head;
     this.head = newNode;
   }
+
+  // insert a node at the end of the list
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+      return;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = newNode;
+  }
+
+  //
 }
 
 const linkedlist = new Linkedlist();
 linkedlist.prepend('test1');
+linkedlist.append('test2');
 console.log(linkedlist);
